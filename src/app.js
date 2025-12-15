60a3ac46bd15480f0f490af7720ad3b80929bccc
SamratJungKatuwal <katuwalsamrat22@gmail.com>
2025-12-15 09:16:53 +0545

    MongoDB connected successfully

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import config from "./config/config.js";
+import connectionDB from "./config/database.js";
 
 const app = express();
+
+connectionDB();
 
 app.use(bodyParser.json());
 
 app.listen(config.port, ()=>{
     console.log(`Server is running at port ${config.port}.....`);
-})
+});
--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+    mongodbUrl: process.env.MONGODB_URL || ""
 }
 
 export default config;
